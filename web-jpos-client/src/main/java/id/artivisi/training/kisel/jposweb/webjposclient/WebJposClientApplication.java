06544ca442b48d16f50e3ad96268e9662204014c
Endy Muhardin <endy.muhardin@gmail.com>
2017-10-20 14:31:14 +0700

    starter project

---
+++ web-jpos-client/src/main/java/id/artivisi/training/kisel/jposweb/webjposclient/WebJposClientApplication.java
@@ -0,0 +1,12 @@
+package id.artivisi.training.kisel.jposweb.webjposclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJposClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebJposClientApplication.class, args);
+	}
+}
